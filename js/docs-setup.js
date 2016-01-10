4e5b5246a2b1b6e5092b3ead7f775ad2afd0ebbc
Huy Dang Lê-Ngô <huy-dang.le-ngo@polymtl.ca>
2016-01-10 08:21:43 -0500

    [DOCS] Incomplete version of the documentation

---
+++ js/docs-setup.js
@@ -0,0 +1,112 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "packery-angular",
+      "shortName": "packery-angular",
+      "type": "overview",
+      "moduleName": "packery-angular",
+      "shortDescription": "packery-angular",
+      "keywords": "add angular api app constants dependency depends directives draggabilly expose exposed functionalities html imported include internally js libraries min module ng-app overview packery packery-angular pkgd project properly script src var work"
+    },
+    {
+      "section": "api",
+      "id": "packery-angular.constant:$paDraggabilly",
+      "shortName": "$paDraggabilly",
+      "type": "function",
+      "moduleName": "packery-angular",
+      "shortDescription": "This AngularJS constant is an interface to the Draggabilly function",
+      "keywords": "angularjs api constant desandro draggabilly exposed function interface library packery-angular property window"
+    },
+    {
+      "section": "api",
+      "id": "packery-angular.constant:$paEvents",
+      "shortName": "$paEvents",
+      "type": "object",
+      "moduleName": "packery-angular",
+      "keywords": "api constant object packery-angular"
+    },
+    {
+      "section": "api",
+      "id": "packery-angular.constant:$paOptions",
+      "shortName": "$paOptions",
+      "type": "object",
+      "moduleName": "packery-angular",
+      "shortDescription": "This service is an AngularJS constant that correspond to the default",
+      "keywords": "add angularjs api appending behavior columnwidth constant control correspond default desandro draggabilly draggable dragselector follow handle html instances isappended isdraggable itemselector metafizzy object option options pa-item pa-stamp packery packery-angular prepending properties_columnwidth properties_dragselector properties_isappended properties_isdraggable properties_itemselector properties_rowheight properties_stamp property rowheight service stamp toggle true values"
+    },
+    {
+      "section": "api",
+      "id": "packery-angular.constant:$paPackery",
+      "shortName": "$paPackery",
+      "type": "function",
+      "moduleName": "packery-angular",
+      "shortDescription": "This AngularJS constant is an interface to the Packery function",
+      "keywords": "angularjs api constant exposed function interface library metafizzy packery packery-angular property window"
+    },
+    {
+      "section": "api",
+      "id": "packery-angular.controller:paPackery",
+      "shortName": "paPackery",
+      "type": "controller",
+      "moduleName": "packery-angular",
+      "keywords": "api controller packery-angular"
+    },
+    {
+      "section": "api",
+      "id": "packery-angular.directive:paPackery",
+      "shortName": "paPackery",
+      "type": "directive",
+      "moduleName": "packery-angular",
+      "keywords": "api directive packery-angular"
+    },
+    {
+      "section": "api",
+      "id": "packery-angular.directive:paPackeryItem",
+      "shortName": "paPackeryItem",
+      "type": "directive",
+      "moduleName": "packery-angular",
+      "keywords": "api directive packery-angular"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api/packery-angular",
+    "scripts": [
+      "js/angular.min.js",
+      "js/angular-animate.min.js",
+      "js/marked.js",
+      "http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js",
+      "http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular-animate.min.js"
+    ],
+    "styles": [],
+    "title": "API Documentation",
+    "html5Mode": false,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {},
+    "titleLink": "https://github.com/Dilatorily/packery-angular",
+    "loadDefaults": {
+      "angular": true,
+      "angularAnimate": true,
+      "marked": true
+    }
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api/packery-angular",
+  "scripts": [
+    "js/angular.min.js",
+    "js/angular-animate.min.js",
+    "js/marked.js",
+    "http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js",
+    "http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular-animate.min.js"
+  ]
+};
